98dbaa49b85e891ccfea725d1af7b1a21c26bad9
Munteanu <munteanuvictoria1@gmail.com>
2024-02-14 20:20:17 +0100

    add resetBoard and shiffle

--- script.js
+++ script.js
@@ -1,28 +1,71 @@
 const cards = document.querySelectorAll('.memory-card');
 let flippedCard = false;
 let firstCard, secondCard; 
+let lockBoard = false;
 
 
 function flipCard(){
+if(lockBoard) return;
+if(this  === firstCard) return; 
+
 this.classList.toggle('flip')
 if(!flippedCard){
     //first click
     flippedCard = true;
-    firstCard = this; 
-    
+    firstCard = this;  
     //second click
-}else {
+} else {
     flippedCard = false;
     secondCard = this;
-
     //check if cards match
-console.log(firstCard.data.character)
-console.log(secondCard.data.character);
-
-}
-// console.log(firstCard, secondCard);
+    checkForMatch();
 
 };
+}
 
 cards.forEach(card => card.addEventListener('click', flipCard)); 
 
+
+function disableCards(){
+    firstCard.removeEventListener('click', flipCard);
+    secondCard.removeEventListener('click',flipCard);
+
+    resetBoard(); 
+};
+
+function unflipCards(){
+    lockBoard = true;
+    setTimeout(() => {
+    firstCard.classList.remove('flip');
+    secondCard.classList.remove('flip');
+    resetBoard();
+     }, 1500); 
+};
+
+function checkForMatch(){
+   let isMatch = firstCard.dataset.character === secondCard.dataset.character;
+   isMatch ? disableCards :  unflipCards();
+
+    // if(firstCard.dataset.character === secondCard.dataset.character){
+    //     //match 
+    //     disableCards();
+    // } else{
+    //     //not a match 
+    //     unflipCards(); 
+    
+    // }
+}; 
+
+function resetBoard(){
+    [flippedCard, lockBoard] = [false, false];
+    [firstCard, secondCard] = [null, null];
+};
+
+(function suffleCards(){
+    cards.forEach(card => {
+        let randomNr = Math.floor(Math.random()*12);
+        card.computedStyleMap.order = randomNr;
+    });
+})();
+
+//immediatly invoked => right after it's definitions 
